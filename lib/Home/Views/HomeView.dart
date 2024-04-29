bcc44abadc02538db49e6800039029f35b4aa8a5
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-04-30 01:21:01 +0900

    feat: 홈 -> 온보딩 구조 구현

---
+++ lib/Home/Views/HomeView.dart
@@ -0,0 +1,46 @@
+import 'package:cotato4cut_narong/Onboarding/Views/OnboardingView.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '홈 화면입니다',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset(
+                'assets/logo1.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => OnboardingView()),
+                  );
+                },
+                child: const Text('시작하기'))
+          ],
+        ),
+      ),
+    );
+  }
+}
